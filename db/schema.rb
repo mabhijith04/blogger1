d4128ff44318b423a42e2eaef9b7c17597d2686f
Venkatesh Prabhu <venkateshprabhu2@gmail.com>
2013-10-07 17:22:02 +0530

    Sorcery authentication complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131006055551) do
+ActiveRecord::Schema.define(version: 20131007102048) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20131006055551) do
     t.string   "title"
     t.text     "body"
     t.text     "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "authors", force: true do |t|
+    t.string   "username",         null: false
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
